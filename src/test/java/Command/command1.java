1e3e24ef6d43d0cd32c290a7e5be48bd98faed3f
abinash <dj.avinash55@gmail.com>
2023-02-16 12:59:22 +0530

    changes done

--- src/test/java/Command/command1.java
+++ src/test/java/Command/command1.java
@@ -4,6 +4,13 @@ public class command1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=61;
+		int b=62;
+		
+		int c=a*b;
+		
+		System.out.println(c);
 
 	}
 
